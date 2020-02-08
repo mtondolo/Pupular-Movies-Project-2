9d9123f84ecbd6a4de102997f51c807946265101
mtondolo <mtondolo@gmail.com>
2020-02-08 16:14:01 +0200

    Add recycerview adapter

--- app/src/main/java/com/example/popularmovies/MoviesAdapter.java
+++ app/src/main/java/com/example/popularmovies/MoviesAdapter.java
@@ -1,6 +1,10 @@
 package com.example.popularmovies;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -8,7 +12,36 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import butterknife.BindView;
 
-public class MoviesAdapter {
+public class MoviesAdapter extends RecyclerView.Adapter<MoviesAdapter.MoviesAdapterViewHolder> {
+    private String[] mMoviesData;
+
+    @NonNull
+    @Override
+    public MoviesAdapterViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        int layoutIdForListItem = R.layout.movies_list_item;
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View view = inflater.inflate(layoutIdForListItem, parent, false);
+        MoviesAdapterViewHolder viewHolder = new MoviesAdapterViewHolder(view);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MoviesAdapterViewHolder holder, int position) {
+        String movieForThisDataPosition = mMoviesData[position];
+        holder.mMoviewTextView.setText(movieForThisDataPosition);
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mMoviesData == null) {
+            return 0;
+        }
+        return mMoviesData.length;
+    }
+
     public class MoviesAdapterViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.tv_movies_data)
         TextView mMoviewTextView;
@@ -16,5 +49,10 @@ public class MoviesAdapter {
         public MoviesAdapterViewHolder(@NonNull View itemView) {
             super(itemView);
         }
+    }
+
+    void setMoviesData(String[] moviesData) {
+        mMoviesData = moviesData;
+        notifyDataSetChanged();
     }
 }
